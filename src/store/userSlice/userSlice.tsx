0431fe3a8bcd445c7135138e0a9c96266e4e384e
Anna Marrodán <anna.marrodan@gmail.com>
2023-02-21 21:00:19 +0100

    Create userSlice to log user

---
+++ src/store/userSlice/userSlice.tsx
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserState, User } from "./types";
+
+const initialState: UserState = {
+  username: "",
+  token: "",
+  id: "",
+  isLogged: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (currentUserState, action: PayloadAction<User>) => ({
+      ...action.payload,
+      isLogged: true,
+    }),
+  },
+});
+
+export const userReducer = userSlice.reducer;
+export const { loginUser: loginUserActionCreator } = userSlice.actions;
